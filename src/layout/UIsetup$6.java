f9999ffb42015b5e0e4a98f039ace6911e2aecd8
Steve Silber <poseidon.prime@gmail.com>
2017-04-11 16:24:20 -0400

    clean up code
    beginning attempt to bring back this project

---
+++ src/layout/UIsetup$6.java
@@ -0,0 +1,204 @@
+ package layout;
+ 
+ import java.awt.Graphics;
+ import java.awt.Image;
+ import javax.swing.ImageIcon;
+ import javax.swing.JPanel;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ class UIsetup$6
+   extends JPanel
+ {
+   private static final long serialVersionUID = 1L;
+   
+   UIsetup$6(UIsetup paramUIsetup) {}
+   
+   public void paintComponent(Graphics g)
+   {
+     Image img = new ImageIcon(getClass().getResource("outerspace.jpg")).getImage();
+     g.drawImage(img, 0, 0, null);
+   }
+ }
+
+
+/* Location:              C:\Users\Zirconix\Desktop\Computer Science\Java Projects\Java Game\bin\!\layout\UIsetup$6.class
+ * Java compiler version: 6 (50.0)
+ * JD-Core Version:       0.7.1
+ */
