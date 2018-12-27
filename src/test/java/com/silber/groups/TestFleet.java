ef3cdd606855b161cdd914654e6835348eda5979
Steve S <ssilber@uwo.ca>
2018-12-27 15:25:15 -0500

    merged testing classes

--- src/test/java/com/silber/groups/TestFleet.java
+++ src/test/java/com/silber/groups/TestFleet.java
@@ -1,13 +1,12 @@
 package test.groups;
 
-import game.entities.Destroyer;
 import game.groups.*;
+import game.managers.ConfigManager.ShipType;
 import game.players.Player;
 import junit.framework.TestCase;
 
 public class TestFleet extends TestCase
 {
-	
 	protected Fleet attacker;
 	protected ShipInventory defender;
 	protected Player player;
@@ -21,12 +20,18 @@ public class TestFleet extends TestCase
 	
 	public void testAttack() throws Exception
 	{
-		attacker.add(Destroyer.class, 10000);
-		defender.add(Destroyer.class, 10000);
+		attacker.add(ShipType.DESTROYER, 10000);
+		defender.add(ShipType.DESTROYER, 10000);
+		attacker.attack(defender, 1.12);
 		
-		attacker.attack(defender, 1.10);
+		boolean success = true;
 		
-		assertTrue(true);
+		if (attacker.getCount() != 0) success = false;
+		if (defender.getCount() == 0) success = false;
+		
+		System.out.printf("attacker: %d, defender: %d", attacker.getCount(), defender.getCount());
+		
+		assertTrue(success);
 	}
 
 }
